b536ef3497fab943519102dd3fa3f99ff3f065ec
ccm2185 <81339844+ccm2185@users.noreply.github.com>
2022-10-01 14:53:01 -0400

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by CJ Magleby on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
